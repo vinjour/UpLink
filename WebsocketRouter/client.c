e19af0164b0916ca01cedd43e3eb0ce245f57009
vinjour <91549304+vinjour@users.noreply.github.com>
2022-07-06 17:14:42 +0200

    Add files via upload

--- WebsocketRouter/client.c
+++ WebsocketRouter/client.c
@@ -1,34 +1,83 @@
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
 #include <libwebsockets.h>
 
-static struct lws *web_socket = NULL;
+static int destroy_flag = 0;
+static int connection_flag = 0;
 
-#define EXAMPLE_RX_BUFFER_BYTES (10)
+void INT_HANDLER(int signo) {
+	destroy_flag = 1;
+};
 
-static int callback_example(struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len) {
-	switch(reason) {
-		case LWS_CALLBACK_CLIENT_ESTABLISHED:
-			lws_callback_on_writable(wsi);
+struct session_data {
+	int fd;
+};
+
+struct pthread_routine_tool {
+	struct lws_context *context;
+	struct lws *wsi;
+};
+
+int websocket_write_back(struct lws *wsi_in, char *str, int str_size_in) {
+	if (str == NULL || wsi_in == NULL) {
+ 		return -1;
+	}
+	
+	int n;
+	int len;
+	unsigned char *out = NULL;
+
+	if (str_size_in < 1) {
+		len = strlen(str);
+	} else {
+		len = str_size_in;
+	}
+
+ 	out = (unsigned char *)malloc(sizeof(unsigned char)*(LWS_SEND_BUFFER_PRE_PADDING + len + LWS_SEND_BUFFER_POST_PADDING));
+	memcpy (out + LWS_SEND_BUFFER_PRE_PADDING, str, len );		// setup the buffer
+	unsigned char *p = &out[LWS_SEND_BUFFER_PRE_PADDING];
+	n = lws_write(wsi_in, p, len, LWS_WRITE_TEXT);		// write out
+
+	printf("[websocket_write_back] %s\n", str);
+	free(out);		// free the buffer
+
+	return n;
+}
+
+// when an event occurs on a connection under a certain protocol, the ws_callback function executed
+int ws_callback(struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len) {
+
+	switch (reason) {		// reason : the event that caused the callback
+
+		case LWS_CALLBACK_CLIENT_ESTABLISHED:	// callback after connecting to the server
+			printf("[Main Service] Connect with server success.\n");
+			connection_flag = 1;
 			break;
 
-		case LWS_CALLBACK_CLIENT_RECEIVE:
-			// Handle incomming messages here
+		case LWS_CALLBACK_CLIENT_CONNECTION_ERROR:		// callback after an error during connection
+			printf("[Main Service] Connect with server error.\n");
+			destroy_flag = 1;
+			connection_flag = 0;
 			break;
 
-		case LWS_CALLBACK_CLIENT_WRITEABLE: {
-			unsigned char buf[LWS_SEND_BUFFER_PRE_PADDING + EXAMPLE_RX_BUFFER_BYTES + LWS_SEND_BUFFER_POST_PADDING];
-			unsigned char *p = &buf[LWS_SEND_BUFFER_PRE_PADDING];
-			size_t n = sprintf((char *)p, "%u", rand());
-			lws_write(wsi, p, n, LWS_WRITE_TEXT);
+		case LWS_CALLBACK_CLOSED:		// callback after closing the connection
+			printf("[Main Service] LWS_CALLBACK_CLOSED\n");
+			destroy_flag = 1;
+			connection_flag = 0;
 			break;
-		}
 
-		case LWS_CALLBACK_CLOSED:
-		
-		case LWS_CALLBACK_CLIENT_CONNECTION_ERROR:
-			web_socket = NULL;
+		case LWS_CALLBACK_CLIENT_RECEIVE:		// callback after receiving the server data
+			printf("[Main Service] Client received : %s\n", (char *)in);
+			break;
+
+		case LWS_CALLBACK_CLIENT_WRITEABLE:		// callback when this client can send data
+			printf("[Main Service] On writeable is called\n");
+			char msg[128] = "";
+			scanf(" %[^\n]", msg);	// scan the message on the prompt
+			websocket_write_back(wsi, (char *) msg, -1);		// send the message to the server
 			break;
 
 		default:
@@ -38,69 +87,96 @@ static int callback_example(struct lws *wsi, enum lws_callback_reasons reason, v
 	return 0;
 }
 
-enum protocols {
-	PROTOCOL_EXAMPLE = 0,
-	PROTOCOL_COUNT
-};
+void *pthread_routine(void *tool_in) {
+	struct pthread_routine_tool *tool = tool_in;
 
-static struct lws_protocols protocols[] = {		// first protocol must always be HTTP handler
+	while(!connection_flag) {		// waiting for connection with server done
+		usleep(1000*20);
+	}
+
+	//*Send greeting to server*/ 
+	lws_callback_on_writable(tool->wsi);	// trigger a WRITEABLE event callback
+	sleep(1);
+	return 0;
+}
+
+struct lws_protocols protocols[] = {		// first protocol must always be HTTP handler
 	{
-		"example-protocol",	// name
-		callback_example,	// callback
-		0,		// per session data size
-		EXAMPLE_RX_BUFFER_BYTES,		// max frame size / rx buffer
+		"websockets",	// name
+		&ws_callback,	// callback
+		sizeof(struct session_data),		// per session data size
+		0,		// max frame size / rx buffer
 	},
 	{
 		NULL, NULL, 0, 0	// end of list
 	}
 };
 
-int main(int argc, char **argv) {		// server url will be http://192.168.2.195:8000
 
+int main(int argc, char **argv) {
+	
+	struct sigaction act;		// register the signal SIGINT handler
+	act.sa_handler = INT_HANDLER;
+	act.sa_flags = 0;
+	sigemptyset(&act.sa_mask);
+	sigaction( SIGINT, &act, 0);
+
+	struct lws_context *context = NULL;
 	struct lws_context_creation_info info;
-	memset( &info, 0, sizeof(info) );
+	struct lws *wsi = NULL;
 
+	memset(&info, 0, sizeof info);		// parameters used to create context
 	info.port = CONTEXT_PORT_NO_LISTEN;
+	info.iface = NULL;
 	info.protocols = protocols;
+	info.ssl_cert_filepath = NULL;	// ssl support
+	info.ssl_private_key_filepath = NULL;	// ssl support
+	info.extensions = NULL;
 	info.gid = -1;
 	info.uid = -1;
+	info.options = 0;
 
-	struct lws_context *context = lws_create_context(&info);	// create lws context representing this server
+	context = lws_create_context(&info);	// create a websocket handler
+	printf("[Main] context created.\n");
 
 	if (context == NULL) {
-		printf("lws init failed\n");
+		printf("[Main] context is NULL.\n");
 		return -1;
 	}
 
-	printf("Starting server...\n");
-
-	time_t old = 0;
-	
-	while(1) {		// infinite loop, to end this server send SIGTERM (CTRL+C)
-		struct timeval tv;
-		gettimeofday( &tv, NULL );
-
-		if( !web_socket && tv.tv_sec != old ) {	// connect if we are not connected to the server
-			struct lws_client_connect_info ccinfo = {0};
-			ccinfo.context = context;
-			ccinfo.address = "192.168.2.195";
-			ccinfo.port = 8000;
-			ccinfo.path = "/";
-			ccinfo.host = lws_canonical_hostname( context );
-			ccinfo.origin = "origin";
-			ccinfo.protocol = protocols[PROTOCOL_EXAMPLE].name;
-			web_socket = lws_client_connect_via_info(&ccinfo);
-		}
-
-		if( tv.tv_sec != old ) {		// send a random number to the server every second
-			lws_callback_on_writable( web_socket );	
-			old = tv.tv_sec;
-		}
-
-		lws_service(context, 250);	// lws-service will process all waiting events with their callback functions every 250ms
+	if(!wsi) {
+		struct lws_client_connect_info ccinfo = {0};		// Client connection parameters
+		ccinfo.context = context;
+		ccinfo.address = "192.168.2.213";
+		ccinfo.port = 8000;
+		ccinfo.path = "/";
+		ccinfo.host = lws_canonical_hostname( context );
+		ccinfo.origin = "origin";
+		ccinfo.protocol = protocols[0].name;
+		wsi = lws_client_connect_via_info(&ccinfo);		// create a client connection
 	}
 
-	lws_context_destroy(context);
+	if (wsi == NULL) {
+		printf("[Main] wsi create error.\n");
+		return -1;
+	}
+
+	printf("[Main] wsi create success.\n");
+
+	struct pthread_routine_tool tool;
+	tool.wsi = wsi;
+	tool.context = context;
+	pthread_t pid;
+
+	while(!destroy_flag) {
+		lws_service(context, 1000);		// execute the event loop once, and wait for a maximum of 1000 ms
+		pthread_create(&pid, NULL, pthread_routine, &tool);
+		pthread_detach(pid);
+	}
+
+
+	printf("Exiting\n");
+	lws_context_destroy(context);		// destroy the context object
 
 	return 0;
 }
